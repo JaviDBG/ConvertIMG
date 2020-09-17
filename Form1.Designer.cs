e5cc7a5599cb7b3c0764019b93a1c80f81618da6
JaviDBG <djbarriosgomez@gmail.com>
2020-09-17 15:11:06 -0500

    add view

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAbrir.Location = new System.Drawing.Point(162, 102);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(121, 24);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Seleccionar Imagen";
+            this.btnAbrir.UseVisualStyleBackColor = false;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(162, 189);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(162, 243);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(121, 23);
+            this.btnConvertir.TabIndex = 2;
+            this.btnConvertir.Text = "3: Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(117, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2: Selecciona el formato al que deseas convertir";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "1: Selecciona un archivo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(476, 499);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ConvertIMG
 {
     public partial class Form1 : Form
     {
+        string nameFile = "";
+        string formato = "";
         public Form1()
         {
             InitializeComponent();
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png, *.gif, *.bmp) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png; *.gif; *.bmp" ;           
+            openFileDialog1.Multiselect = false;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Text = "<--Formatos-->";
+            comboBox1.Items.Add("jpg");
+            comboBox1.Items.Add("png");
+            comboBox1.Items.Add("gif");
+            comboBox1.Items.Add("bmp");
+            //comboBox1.Enabled = false;
+            
+            btnConvertir.Enabled = false;
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            
+           
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                //nameFile = Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+                    nameFile = openFileDialog1.SafeFileName;
+                    label1.Text ="Archivo '"+nameFile+"' seleccionado ";
+                    comboBox1.Enabled = true;
+                }                
+           
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (btnConvertir.Enabled==false)
+            {
+                btnConvertir.Enabled = true;
+            }
+            formato = comboBox1.Items[comboBox1.SelectedIndex].ToString();
         }
     }
 }
